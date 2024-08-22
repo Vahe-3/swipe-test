dcb18bfbbccb649d19cfa8fef4fd9c96aa4b0cf6
Vahe-3 <vahe.avetisyan333@gmail.com>
2024-08-22 13:41:31 +0400

    disabled app swipe

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-window.Telegram.WebApp.disableVerticalSwipes()
+// window.Telegram.WebApp.disableVerticalSwipes()
 
 createApp(App).mount('#app')
