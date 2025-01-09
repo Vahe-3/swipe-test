d3a5c885d22b6560a96b294bd6f40288ef97b297
Vahe Avetisyan <vahe.avetisyan333@gmail.com>
2025-01-09 13:53:05 +0400

    added expand

--- src/main.js
+++ src/main.js
@@ -5,21 +5,21 @@ import App from './App.vue'
 import Vant from 'vant';
 
 window.Telegram.WebApp.disableVerticalSwipes();
-console.log(window.Telegram.WebApp.version);
+window.Telegram.WebApp.expand();
 
 // Check if the Telegram WebApp supports the addToHomeScreen method
-if (Telegram.WebApp.isExpanded && Telegram.WebApp.canAddToHomeScreen) {
-    // Call the method to prompt the user to add the app to their home screen
-    Telegram.WebApp.addToHomeScreen()
-        .then(() => {
-            console.log("Added to home screen successfully!");
-        })
-        .catch((error) => {
-            console.error("Failed to add to home screen:", error);
-        });
-} else {
-    alert("Add to Home Screen is not supported on this device or environment.");
-}
+// if (Telegram.WebApp.isExpanded && Telegram.WebApp.canAddToHomeScreen) {
+//     // Call the method to prompt the user to add the app to their home screen
+//     Telegram.WebApp.addToHomeScreen()
+//         .then(() => {
+//             console.log("Added to home screen successfully!");
+//         })
+//         .catch((error) => {
+//             console.error("Failed to add to home screen:", error);
+//         });
+// } else {
+//     // alert("Add to Home Screen is not supported on this device or environment.");
+// }
 
 
 
