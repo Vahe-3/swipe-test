16db8933c42b3e4a01fa9d62fd645fd03ba0109c
Vahe <vahe@Vahes-MacBook-Pro.local>
2025-07-24 15:02:27 +0400

    added ads

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,80 @@
 <template>
-  <div class="container">
-    <h1>Hello</h1>
-    <adsgram-task data-block-id="task-13058" data-debug="true" class="task">
-      <template #reward>
-        <h2>Swipe Test</h2>
-      </template>
-    </adsgram-task>
-  </div>
+  <adsgram-task
+    class="task"
+    :data-block-id="blockId"
+    :data-debug="debug"
+    ref="taskRef"
+  >
+    <span slot="reward" class="task__reward">1000 монет</span>
+    <div slot="button" class="task__button">вперед</div>
+    <div slot="claim" class="task__button task__button--claim">получить</div>
+    <div slot="done" class="task__button task__button--done">готово</div>
+  </adsgram-task>
 </template>
 
-<script setup></script>
+<script setup>
+import { onMounted, onBeforeUnmount, ref } from 'vue';
 
-<style scoped lang="less">
-.container {
-  padding: 0 15px 20px 15px;
+const props = defineProps({
+  debug: { type: Boolean, default: false },
+  blockId: { type: String, required: true }
+});
+
+const taskRef = ref(null);
+
+const handler = (event) => {
+  alert(`Награда, detail = ${event.detail}`);
+};
+
+onMounted(() => {
+  if (taskRef.value) {
+    taskRef.value.addEventListener('reward', handler);
+  }
+});
+
+onBeforeUnmount(() => {
+  if (taskRef.value) {
+    taskRef.value.removeEventListener('reward', handler);
+  }
+});
+</script>
+
+<style scoped>
+.task {
+  --adsgram-task-font-size: 16px; /* минимум 14px */
+  --adsgram-task-icon-size: 50px; /* минимум 30px */
+  --adsgram-task-icon-title-gap: 15px; /* минимум 5px максимум 40px */
+  --adsgram-task-button-width: 60px; /* минимум 40px */
+  --adsgram-task-icon-border-radius: 8px;
+
+  display: block;
+  width: 328px;
+  padding: 8px 16px 8px 8px;
+  border-radius: 16px;
+  background-color: #1d2733;
+  font-family: Roboto;
+  color: white;
+}
+
+.task__reward {
+  margin: 5px 0 0 0;
+  font-size: 14px;
+}
+
+.task__button {
+  margin-left: 10px;
+  background-color: #50a8eb;
+  border-radius: 5px;
+  padding: 6px 12px;
+}
+
+.task__button_claim {
+  margin-left: 0;
+  background-color: #ee941c;
+}
+
+.task__button_done {
+  margin-left: 0;
+  background-color: #007539;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -8,48 +8,6 @@ import * as AdsgramVue from '@adsgram/vue';
 // Disable vertical swipes in Telegram Web App
 window.Telegram.WebApp.disableVerticalSwipes();
 
-// Notify Telegram that the Mini App is ready
-function notifyAppReady() {
-    if (window.Telegram && window.Telegram.WebApp) {
-        window.Telegram.WebApp.ready(); // Properly notify Telegram
-    } else {
-        console.error("Telegram WebApp API is not available.");
-    }
-}
-
-// Request full-screen mode
-// function requestFullScreen() {
-    
-//     if (window.Telegram && window.Telegram.WebApp) {        
-//         try {            
-//             window.Telegram.WebApp.requestFullscreen();
-
-            
-//         } catch (error) {
-//             console.error("An error occurred while requesting full-screen mode:", error.message);
-//         }
-//     } 
-// }
-
-
-// requestFullScreen();
-
-// if (window.Telegram.WebApp?.version >= 8.0) {
-//     // Attach the fullscreenChanged event listener
-//     window.Telegram.WebApp.onEvent('fullscreenChanged', () => {
-//         const isFullscreen = window.Telegram.WebApp.isFullscreen;
-
-//         if (isFullscreen) {
-//            alert('App entered fullscreen mode');
-//         } else {
-//             alert('App Not');
-//         }
-//     });
-
-// }
-
-
-
 
 // Create the Vue application
 createApp(App).use(Vant).use(AdsgramVue).mount('#app');
